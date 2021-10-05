6a1f0aec4d5486913d7546ffc9dd034985ac4e6b
neil.warner <neil.warner@everbridge.com>
2021-10-05 11:48:34 -0500

    shows in gutter 93% lines covered, no error

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -1,8 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+// The application under test.
+import 'package:rhf_hw_test2/main.dart' as app;
+
 void main() {
-  testWidgets("failing test example", (WidgetTester tester) async {
-    expect(2 + 2, equals(5));
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button; verify counter',
+            (WidgetTester tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+
+          // Finds the floating action button to tap on.
+          final Finder fab = find.byTooltip('Increment');
+
+          // Emulate a tap on the floating action button.
+          await tester.tap(fab);
+
+          await tester.pumpAndSettle();
+
+          expect(find.text('1'), findsOneWidget);
+        });
   });
 }
