249c58a5da93758647957d1120bcdd90468cef17
neil.warner <neil.warner@everbridge.com>
2021-10-05 14:33:56 -0500

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 Future<Album> fetchAlbum() async {
+  print('using dio');
   final response =
-  await http.get('https://jsonplaceholder.typicode.com/albums/1');
+  await Dio().get('https://jsonplaceholder.typicode.com/albums/1');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
+    return Album.fromJson(jsonDecode(response.data.toString()));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
